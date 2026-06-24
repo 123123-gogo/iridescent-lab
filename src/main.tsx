7832643490f29b6c8b1f446e2746f2e372f5dc93
Showmaker <showmaker@jingdu.dev>
2026-06-24 12:51:51 +0800

    fix: github pages base path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/iridescent-lab">
       <App />
     </BrowserRouter>
   </StrictMode>
